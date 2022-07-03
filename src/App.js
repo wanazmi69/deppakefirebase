f0258dc8247e324ff3d8bc897c277b72235a3a3d
wanazmi69 <lawtheoscar@gmail.com>
2022-07-03 13:09:16 +0900

    commit new

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex justify-center items-center w-full h-screen bg-sky-400">
-      <span>Tes Tailwind</span>
+    <div className="flex flex-col justify-center items-center w-full h-screen bg-sky-400">
+      <span className="text-4xl font-extrabold text-white">Tes Tailwind</span>
+      <span >deploy menggunakan Github</span>
     </div>
   );
 }
